66ba421d10122b377425ce2603c41ce3ca9ae165
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2021-04-18 19:18:19 +0300

    remove web and text modules

--- text.py
+++
@@ -1,6 +0,0 @@
-def text_mode(text):
-    text_variations = []
-    text = text.capitalize()
-    return text
-
-print((text_mode('pewdiepie')))
--- web.py
+++
@@ -1,2 +0,0 @@
-import webbrowser
-webbrowser.open_new_tab('https://www.youtube.com/')
